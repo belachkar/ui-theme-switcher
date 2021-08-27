95828d570905940460ae8ae710c7822d3c81e3a6
belachkar <belachkar_ali@hotmail.com>
2021-08-27 03:59:58 +0100

    Add: - JS themes switching onClick events - LocalStorage integration

---
+++ app.js
@@ -0,0 +1,30 @@
+const darkBtn = document.getElementById('dark');
+const lightBtn = document.getElementById('light');
+const solarBtn = document.getElementById('solar');
+const body = document.body;
+
+// Apply the saved theme on reload
+const theme = localStorage.getItem('theme');
+const isSolar = JSON.parse(localStorage.getItem('isSolar'));
+
+// Apply saved themes
+theme && body.classList.add('theme', theme);
+theme && isSolar && body.classList.add('solar');
+
+lightBtn.onclick = () => {
+  body.classList.replace('dark', 'light');
+  localStorage.setItem('theme', 'light');
+};
+
+darkBtn.onclick = () => {
+  body.classList.replace('light', 'dark');
+  localStorage.setItem('theme', 'dark');
+};
+
+solarBtn.onclick = () => {
+  const isSolarNow = body.classList.toggle('solar');
+  const solarBtnText = isSolarNow ? 'Normalize' : 'Solarize';
+
+  solarBtn.innerText = solarBtnText;
+  localStorage.setItem('isSolar', JSON.stringify(isSolarNow));
+};
